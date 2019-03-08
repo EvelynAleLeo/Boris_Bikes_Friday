be1cb951bba9c63746d1245691e33f777faca3e0
Evelyn <evelyn@ipsoft.es>
2019-03-08 15:54:13 +0000

    Writen text for error messages waiting to write code

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,8 @@ require 'docking_station'
 
 describe DockingStation do
   it 'creates new DockingStation' do
-    expect(DockingStation.new.is_a? DockingStation).to eq true
+    #expect(DockingStation.new.is_a? DockingStation).to eq true
+    expect(DockingStation.new).to be_a DockingStation
   end
 
 end
@@ -41,5 +42,12 @@ describe DockingStation do
     bike = Bike.new
     docking_station.dock(bike)
     expect(docking_station.bike).to eq bike
+  end
+end
+
+describe DockingStation do
+  it 'gives no bikes available error message' do
+    docking_station = DockingStation.new
+    expect {docking_station.release_bike}.to raise_error "No more bikes available"
   end
 end
